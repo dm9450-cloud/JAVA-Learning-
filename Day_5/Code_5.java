9facea69d5617dccff4e05d843c0f40b8d9124e6
Dipanshu Maddheshiya <70335021+dm9450-cloud@users.noreply.github.com>
2024-08-10 22:24:21 +0530

    Day_5(Input in java)

---
+++ Day_5/Code_5.java
@@ -0,0 +1,10 @@
+package Day_5;
+
+public class Code_5 {
+    public static void main(String[] args) {
+        int a=2, b=4, c=6, x=9, y=12;
+        int exp = 4 / 3 * x + 34 + 9 * a + b * c + 3 + y * 2 + a / a + b * y;
+                //   4 / 3 * 9 + 34 + 9 * 2 + 4 * 6 + 3 + 12 * 2 + 2 / 2 + 4 * 12;
+        System.out.println(exp);
+    }
+}
