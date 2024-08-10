0d2072bb83a02da117cdf4c44fab2db62f2f83ab
Dipanshu Maddheshiya <70335021+dm9450-cloud@users.noreply.github.com>
2024-08-10 22:58:27 +0530

    Delete Day_5 directory

--- Day_5/Code_1.java
+++
@@ -1,9 +0,0 @@
-package Day_5;
-
-public class Code_1 {
-    public static void main(String[] args) {
-        System.out.println((3>2) && (15<10));  //false
-        System.out.println(true && true);  //true
-        // System.out.println(3>2);  //we can't use
-    }
-}
--- Day_5/Code_3.java
+++
@@ -1,8 +0,0 @@
-package Day_5;
-
-public class Code_3 {
-    public static void main(String[] args) {
-        System.out.println(!(3>12));   //true
-        
-    }
-}
