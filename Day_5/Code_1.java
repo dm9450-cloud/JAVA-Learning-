4dad4dc1402fefee07d01c4402410024a0ea7850
Dipanshu Maddheshiya <70335021+dm9450-cloud@users.noreply.github.com>
2024-08-10 22:59:08 +0530

    Day_5(Input in java)

---
+++ Day_5/Code_1.java
@@ -0,0 +1,9 @@
+package Day_5;
+
+public class Code_1 {
+    public static void main(String[] args) {
+        System.out.println((3>2) && (15<10));  //false
+        System.out.println(true && true);  //true
+        // System.out.println(3>2);  //we can't use
+    }
+}
---
+++ Day_5/Code_2.java
@@ -0,0 +1,8 @@
+package Day_5;
+
+public class Code_2 {
+    public static void main(String[] args) {
+        System.out.println((3>2) || (5<10));  //true
+        System.out.println((3>12) || (15<10));  //false
+    }
+}
