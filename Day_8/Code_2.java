ce9708a3a4a7825b8d79d12c8453ab65e3f0cad2
Dipanshu Maddheshiya <70335021+dm9450-cloud@users.noreply.github.com>
2024-08-13 21:47:17 +0530

    Day_8(revision)

---
+++ Day_8/Code_2.java
@@ -0,0 +1,59 @@
+// Switch Case Problem 2
+// You are given a positive integer N. Write a program using switch case such that :
+
+// if user enters 12 , print "I am even",
+
+// if user enters 13 , print "I am prime",
+
+// if user enters 25 , print "I am odd",
+
+// Otherwise , print "I am just a number".
+
+// Input Format
+// First line contains an integer N.
+
+// Output Format
+// Print the sentence related to the integer as per description.
+
+// Example 1
+// Input
+
+// 25
+// Output
+
+// I am odd
+// Example 2
+// Input
+
+// 87
+// Output
+
+// I am just a number
+// Constraints
+// 1 <= N <= 10^6
+
+// Topics
+// Basics
+
+
+package Day_8;
+
+import java.util.*;
+
+public class Code_2 {
+    public static void main(String args[]) throws Throwable{
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        //Write code here and print output here
+        switch(N){
+            case 12 : System.out.println("I am even");
+            break;
+            case 13 : System.out.println("I am prime");
+            break;
+            case 25 : System.out.println("I am odd");
+            break;
+            default : System.out.println("I am just a number");
+            
+        }
+    }
+}
